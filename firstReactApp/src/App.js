40300c0e7a7cbcbb42dbb838adedcc19097b3e79
shivani89 <shivani.ui89@gmail.com>
2017-01-29 12:32:06 -0500

    refs and events

--- firstReactApp/src/App.js
+++ firstReactApp/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 //stateful component- which has a state whic changes dynamically. here the src logo might change.
 //create an component, header-img, footer, body-p
 //in here we write our components
@@ -16,6 +17,7 @@ class App extends Component {
       <div className="App">
         <Header/>
         <Content/>
+      
         <Footer/>
       </div>
 
@@ -29,8 +31,7 @@ class Header extends Component{ /*/*this is the syntax for component creatin wit
 
     return (
       <div className="App-header">
-          
-          <header-img src="http://codecondo.com/wp-content/uploads/2015/05/Resources-to-Get-You-Started-with-ReactJS.jpg?478983" />
+          <h2>This is the header section</h2> 
       </div>
       );
   }/*this render method will help in parsing html inside this js code*/
@@ -48,12 +49,47 @@ class Content extends Component{
         {"name":"John", "age":"20" },
         {"name":"SAm", "age":"30" },
         {"name":"Pam", "age":"23" },
+        {"name":"Pam", "age":"28" },
         {"name":"Julie", "age":"24" }
        
     ]
-  
-  }
+   }
 }
+  
+
+
+
+ changeContent(e) {
+
+   
+    console.log('Name changed to '+ this.refs.name.value);
+
+    console.log('Age chnaged to '+ this.refs.age.value);
+
+ }
+
+
+
+  sendContent() {
+  
+ /*Object person = new Object()*/
+  
+var person ={}
+
+person.name = this.refs.name.value,
+person.age = this.refs.age.value,
+
+console.log(person)
+
+var newPerson = this.state.data.slice()
+newPerson.push(person)
+this.setState({ data: newPerson })
+    
+ 
+
+  } 
+
+   
   render () {
 
     return (
@@ -61,9 +97,20 @@ class Content extends Component{
           <h2>This is the Content section with a data</h2> 
           <table>
             <tbody>
-            {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+            {this.state.data.map(
+              (person, i) => <TableRow  key= {i} data = {person} />
+              )
+            }
             </tbody>           
           </table>
+
+          <div>
+            name:<input type="text" ref="name"  onChange={this.changeContent.bind(this)}/>
+            age:<input type="text" ref="age"  onChange={this.changeContent.bind(this)} />
+            <button onClick={this.sendContent.bind(this)}>submit</button> 
+          </div>
+
+        
       </div>
       );
   }
@@ -91,11 +138,13 @@ class TableRow  extends Component{ /*this component is used in content component
         <td>{this.props.data.name}</td>   
         <td>{this.props.data.age}</td>
       </tr>
+
       );
   }
 }
-       
-     
+
+
+
 
 export default App;
 
